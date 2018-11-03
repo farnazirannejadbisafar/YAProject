74a1958a2a48875372a2361976f59d52b4d70f59
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-11-03 01:09:18 -0400

    updated method

--- app.js
+++ app.js
@@ -98,19 +98,19 @@ app.use("/api/user", userRoutes);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
+// app.use((req, res, next) => {
+//   const error = new Error("Not found");
+//   error.status = 404;
+//   next(error);
+// });
+//
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   });
+// });
 
 module.exports = app;
