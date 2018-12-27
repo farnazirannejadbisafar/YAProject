145ceeccc6c2221cb9a7144160e0dce0fda20075
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-12-27 03:18:48 -0500

    Changed shape

--- public/app.js
+++ public/app.js
@@ -1675,14 +1675,14 @@
                                 .data(acquaintances_all_followers)
                                 // .enter().append("circle")
                                 .enter().append("path")
-                                .attr("d", function(d) {
+                                .attr("d", function(d, i) {
                                     if(i === middle){
                                         return d3.symbol().type(d3.symbolCross).size(100);
                                     }
                                     else if(d[2] <= maxFollower && d[2] >= minFollower){
                                         return d3.symbol().type(d3.symbolTriangle).size(100);
                                     }
-                                    return d3.symbol().type(d3.symbolDiamond).size(100)
+                                    return d3.symbol().type(d3.symbolCircle).size(100)
 
                                 })
                                 .attr('id', function(d){ return 'name' + d[2]; })
